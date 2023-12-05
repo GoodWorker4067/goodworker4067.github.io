eaa728cc7b4cd8898a0d1de7485bb0c8b268e247
AntonPractice <npr.haustov@gmail.com>
2023-12-05 23:14:36 +0300

    test2

--- src/App.tsx
+++ src/App.tsx
@@ -7,7 +7,7 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-      <p>О себе:<p/>
+      <p>О себе:</p>
       </header>
     </div>
   );
